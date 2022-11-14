f449549f282ed067211285770b86725057b25921
MiroslaVv <52395254+NoCodin@users.noreply.github.com>
2022-11-14 17:41:40 +0100

    Portfolio controller (#3)
    * front init
    
    * Added Portfolio Module
    
    * update
    
    * removed unecessary files
    
    * bug fixes
    
    * bug fixes
    
    * App module fix
    
    Co-authored-by: MiroslaVV <shlyify@gmail.com>
    Co-authored-by: redzimskidev <redzimski@hotmail.com>

---
+++ src/backend/devolio-api/src/portfolio/portfolio.controller.ts
@@ -0,0 +1,46 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreatePortfolioBodyDto } from './dto/create-portfolio.dto';
+import { UpdatePortfolioBodyDto } from './dto/update-portfolio.dto';
+import { Portfolio } from './portfolio.interface';
+import { PortfolioService } from './portfolio.service';
+
+@Controller('portfolio')
+export class PortfolioController {
+  constructor(private portfiolioService: PortfolioService) {}
+
+  @Get()
+  getAllPortfolios(): Portfolio[] {
+    return this.portfiolioService.findAll();
+  }
+
+  @Get(':id')
+  findOneById(@Param('id') id: string) {
+    return this.portfiolioService.findById(Number(id));
+  }
+
+  @Post()
+  createPortfolio(@Body() body: CreatePortfolioBodyDto) {
+    return this.portfiolioService.createPortfolio(body);
+  }
+
+  @Delete(':id')
+  deletPortfolio(@Param('id') id) {
+    this.portfiolioService.deletePortfolio(id);
+  }
+
+  @Put(':id')
+  updatePortfolio(
+    @Body() updatePortfolio: UpdatePortfolioBodyDto,
+    @Param('id') id,
+  ) {
+    return this.portfiolioService.updatePortfolio(id, updatePortfolio);
+  }
+}
---
+++ src/backend/devolio-api/src/portfolio/portfolio.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@nestjs/common';
+import { identity } from 'rxjs';
+import { developerType } from './developer-type.enum';
+import { CreatePortfolioBodyDto } from './dto/create-portfolio.dto';
+import { UpdatePortfolioBodyDto } from './dto/update-portfolio.dto';
+import { Portfolio } from './portfolio.interface';
+
+@Injectable()
+export class PortfolioService {
+  potfolioUno: Portfolio = {
+    id: 1,
+    developerName: 'Mariusz',
+    bio: 'jakie bio kur?',
+    developerType: developerType.BACKEND,
+    workExperiences: ['raz', 'dwa', 'trzy'],
+    knownTechnologies: ['java', 'python'],
+  };
+  potfolioDuo: Portfolio = {
+    id: 2,
+    developerName: 'Miroslaw',
+    bio: 'łymen',
+    developerType: developerType.FULLSTACK,
+    workExperiences: ['razraz', 'dwadwa', 'trzyrzs'],
+    knownTechnologies: ['javascript', 'html'],
+  };
+  portfolios = [this.potfolioUno, this.potfolioDuo];
+
+  findAll() {
+    return this.portfolios;
+  }
+
+  findById(portfolioId: number) {
+    return this.portfolios.find((id) => id.id === portfolioId);
+  }
+
+  createPortfolio(createPortfolio: CreatePortfolioBodyDto) {
+    const newPortfolio = this.portfolioDtoToPortfolio(createPortfolio);
+    this.portfolios.push(newPortfolio);
+    return newPortfolio;
+  }
+
+  deletePortfolio(portfolioId: number) {}
+
+  updatePortfolio(portfolioId: number, body: UpdatePortfolioBodyDto) {
+    return this.portfolios.find((id) => id.id === portfolioId);
+  }
+
+  portfolioDtoToPortfolio(createPortfolio: CreatePortfolioBodyDto): Portfolio {
+    const newPortfolio: Portfolio = {
+      developerName: createPortfolio.developerName,
+      bio: createPortfolio.bio,
+      developerType: createPortfolio.developerType,
+      workExperiences: createPortfolio.workExperiences,
+      knownTechnologies: createPortfolio.knownTechnologies,
+    };
+    return newPortfolio;
+  }
+}
