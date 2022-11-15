25c4a534df1d29205e8ec92b0584a82b66fac792
Miroslaw <miroslaw.wlaszczuk@gmail.com>
2022-11-15 15:07:15 +0100

    Add PortfolioService with models

--- src/backend/devolio-api/src/main.ts
+++ src/backend/devolio-api/src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {PortfolioModule} from "./portfolio/portfolio.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(PortfolioModule);
   await app.listen(3000);
 }
 bootstrap();
---
+++ src/frontend/DevolioFront/src/app/portfolio/portfolio.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Portfolio } from './dtos/create-portfolio.dto';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PortfolioService {
+  constructor(private http: HttpClient) {}
+  private apiUrl = 'http://localhost:3000/';
+
+  getPortfolios(): Observable<Portfolio[]> {
+    return this.http.get<Portfolio[]>(this.apiUrl);
+  }
+
+  deletePortfolio(portfolio: Portfolio): Observable<Portfolio> {
+    const url = `${this.apiUrl}/${portfolio.id}`;
+    return this.http.delete<Portfolio>(url);
+  }
+
+  updatePortfolio(portfolio: Portfolio): Observable<Portfolio> {
+    const url = `${this.apiUrl}/${portfolio.id}`;
+    return this.http.put<Portfolio>(url, portfolio);
+  }
+
+  addPortfolio(portfolio: Portfolio): Observable<Portfolio> {
+    return this.http.post<Portfolio>(this.apiUrl, portfolio);
+  }
+}
