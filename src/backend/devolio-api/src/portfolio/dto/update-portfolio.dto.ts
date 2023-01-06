05932e50149456b81c24708ef8f2945175b6234e
Miroslaw <miroslaw.wlaszczuk@gmail.com>
2023-01-06 23:41:04 +0100

    Fixes

--- src/backend/devolio-api/src/portfolio/dto/update-portfolio.dto.ts
+++ src/backend/devolio-api/src/portfolio/dto/update-portfolio.dto.ts
@@ -2,12 +2,7 @@ import { IsNotEmpty } from 'class-validator';
 import { developerType } from '../developer-type.enum';
 
 export class UpdatePortfolioBodyDto {
-  // readonly developerName: string;
-  // readonly bio: string;
-  // readonly developerType: developerType;
-  // readonly workExperiences: string[];
-  // readonly knownTechnologies: string[];
-  // @IsNotEmpty()
+  @IsNotEmpty()
   developerName: string;
   @IsNotEmpty()
   bio: string;
