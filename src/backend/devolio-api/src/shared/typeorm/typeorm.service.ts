6a48009c86a8fc404a50532538f8ece732bcf631
redzimskidev <11840988+redzimskidev@users.noreply.github.com>
2023-03-25 12:50:09 +0100

    Connect to fly.io db

--- src/backend/devolio-api/src/shared/typeorm/typeorm.service.ts
+++ src/backend/devolio-api/src/shared/typeorm/typeorm.service.ts
@@ -8,13 +8,23 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   private readonly config: ConfigService;
 
   public createTypeOrmOptions(): TypeOrmModuleOptions {
+    const DB_URL = process.env.DATABASE_URL;
+
+    let partialOptions = DB_URL
+      ? {
+          url: DB_URL,
+        }
+      : {
+          host: this.config.get<string>('DATABASE_HOST'),
+          port: this.config.get<number>('DATABASE_PORT'),
+          database: this.config.get<string>('DATABASE_NAME'),
+          username: this.config.get<string>('DATABASE_USER'),
+          password: this.config.get<string>('DATABASE_PASSWORD'),
+        };
     return {
+      ...partialOptions,
       type: 'postgres',
-      host: this.config.get<string>('DATABASE_HOST'),
-      port: this.config.get<number>('DATABASE_PORT'),
-      database: this.config.get<string>('DATABASE_NAME'),
-      username: this.config.get<string>('DATABASE_USER'),
-      password: this.config.get<string>('DATABASE_PASSWORD'),
+
       entities: ['dist/**/*.entity.{ts,js}'],
       migrations: ['dist/migrations/*.{ts,js}'],
       migrationsTableName: 'typeorm_migrations',
